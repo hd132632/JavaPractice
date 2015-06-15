d561608ef92dcbcc680be9ebb3c3ff1b4fc73435
hd132632 <cys425922@naver.com>
2015-06-15 16:24:42 +0900

    d

---
+++ JavaPractice/src/org/dimigo/oop/PiggyBankTest.java
@@ -0,0 +1,23 @@
+package org.dimigo.oop;
+
+public class PiggyBankTest {
+
+	public static void main(String[] args) {
+		FamilyMember dady = new FamilyMember("아빠");
+		FamilyMember mom = new FamilyMember("엄마");
+		FamilyMember me = new FamilyMember("나");
+		FamilyMember brother = new FamilyMember("남동생");
+		
+		PiggyBank.putMoney(dady, 10000);
+		PiggyBank.putMoney(mom, 5000);
+		PiggyBank.putMoney(me, 2000);
+		PiggyBank.putMoney(brother, 1000);
+		PiggyBank.printBalance();
+		PiggyBank.putMoney(me, 1000);
+		PiggyBank.printBalance();
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
